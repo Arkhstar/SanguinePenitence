d875ab58910038bedbebe5e514113d13a11738df
Arkhstar <bencoberly@icloud.com>
2025-09-23 16:56:30 -0500

    Environment

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,3 +1,4 @@
+class_name PlayerController
 extends CharacterBody2D
 
 var ignore_input : bool = false
@@ -13,12 +14,3 @@ func _physics_process(_delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
-	
-	if position.x > 256.:
-		position.x -= 256.
-	elif position.x < 0:
-		position.x += 256
-	if position.y > 240.:
-		position.y -= 240.
-	elif position.y < 0:
-		position.y += 240.
