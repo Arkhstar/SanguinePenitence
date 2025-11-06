1ca0e081a22777c847438d93c021587498f7e563
Arkhstar <bencoberly@icloud.com>
2025-11-06 15:30:32 -0600

    Battle Background

--- Scripts/Overworld/Monsters/Monster.gd
+++ Scripts/Overworld/Monsters/Monster.gd
@@ -7,10 +7,12 @@ func _on_area_entered(_body: Node2D) -> void:
 	OverworldPlayer.i.ignore_input = true
 	MusicStreamPlayer.adjust_volume(0.0, 1.0)
 	initiate_combat_sfx.play()
+	OverworldPlayer.i.cam.zoom = Vector2(1.002, 1.002)
+	OverworldPlayer.i.cam.rotation_degrees = 0.1 * (1.0 if OverworldPlayer.i.sprite.flip_h else -1.0)
 	TransitionScreen.fade_in(2.5)
 	while initiate_combat_sfx.playing:
-		OverworldPlayer.i.cam.rotation_degrees += 0.25 * (1.0 if OverworldPlayer.i.sprite.flip_h else -1.0);
-		OverworldPlayer.i.cam.zoom += Vector2(0.005, 0.005);
+		OverworldPlayer.i.cam.rotation_degrees += OverworldPlayer.i.cam.rotation_degrees / 16.0;
+		OverworldPlayer.i.cam.zoom += Vector2(OverworldPlayer.i.cam.zoom.x - 1.0, OverworldPlayer.i.cam.zoom.y - 1.0) / 16.0;
 		await RenderingServer.frame_post_draw
 	Main.i.call_deferred("change_to_battle_from_overworld", [ EnemyUnit.new("TEST CASE", 50, 12, 6, 0.0, 1.0, 1.0, EnemyUnit.TargetingType.LOW_HEALTH) ], MusicStreamPlayer.Song.BATTLE3)
 	queue_free()
