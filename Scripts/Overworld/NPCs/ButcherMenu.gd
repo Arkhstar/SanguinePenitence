78cf5af7c7b4d1dff0fda7441f594f262eb9f2af
Arkhstar <bencoberly@icloud.com>
2025-11-15 12:11:56 -0600

    update butchery menu

--- Scripts/Overworld/NPCs/ButcherMenu.gd
+++ Scripts/Overworld/NPCs/ButcherMenu.gd
@@ -7,6 +7,7 @@ extends NPCMenu
 func _ready() -> void:
 	sell.exit.connect(func() -> void:
 		sell.close()
+		dialogue.show()
 		await dialogue.speak("THE BUTCHER", ["... ASK NO QUESTIONS AND NONE WILL BE ASKED OF YOU ...","... THANKS FOR DOING BUSINESS ..."][randi_range(0,1)])
 		close())
 
@@ -16,6 +17,7 @@ func init() -> void:
 		await dialogue.speak("THE BUTCHER", "... COME BACK WITH SOMETHING ...")
 	else:
 		if await dialogue.speak("THE BUTCHER", "... WHAT ARE YOU OFFERING? ...", ["SELL", "EXIT"]) == 0:
+			dialogue.hide()
 			sell.open()
 			return
 	close()
