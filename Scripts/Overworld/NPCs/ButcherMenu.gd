1490e02d6fc6c848ea87dafe40d139988bf8d72a
Arkhstar <bencoberly@icloud.com>
2025-11-15 19:32:10 -0600

    Name the NPCs
    Sefirot!

--- Scripts/Overworld/NPCs/ButcherMenu.gd
+++ Scripts/Overworld/NPCs/ButcherMenu.gd
@@ -4,19 +4,23 @@ extends NPCMenu
 @onready var dialogue : DialogueMenu = $DialogueMenu
 @onready var sell : SellMenu = $SellMenu
 
+var display_name : String = "THE BUTCHER"
+
 func _ready() -> void:
 	sell.exit.connect(func() -> void:
 		sell.close()
 		dialogue.show()
-		await dialogue.speak("THE BUTCHER", ["... ASK NO QUESTIONS AND NONE WILL BE ASKED OF YOU ...","... THANKS FOR DOING BUSINESS ..."][randi_range(0,1)])
+		await dialogue.speak(display_name, ["... ASK NO QUESTIONS AND NONE WILL BE ASKED OF YOU ...","... THANKS FOR DOING BUSINESS ..."][randi_range(0,1)])
 		close())
 
 func init() -> void:
+	if SaveData.townsfolk & 2:
+		display_name = "HESSYN, THE BUTCHER"
 	if SaveData.inventory.monster_parts.count(0) == 5:
-		await dialogue.speak("THE BUTCHER", "... YOU HAVE NOTHING TO OFFER ...")
-		await dialogue.speak("THE BUTCHER", "... COME BACK WITH SOMETHING ...")
+		await dialogue.speak(display_name, "... YOU HAVE NOTHING TO OFFER ...")
+		await dialogue.speak(display_name, "... COME BACK WITH SOMETHING ...")
 	else:
-		if await dialogue.speak("THE BUTCHER", "... WHAT ARE YOU OFFERING? ...", ["SELL", "EXIT"]) == 0:
+		if await dialogue.speak(display_name, "... WHAT ARE YOU OFFERING? ...", ["SELL", "EXIT"]) == 0:
 			dialogue.hide()
 			sell.open()
 			return
--- Scripts/Overworld/NPCs/SmithMenu.gd
+++ Scripts/Overworld/NPCs/SmithMenu.gd
@@ -4,15 +4,19 @@ extends NPCMenu
 @onready var dialogue : DialogueMenu = $DialogueMenu
 @onready var shop : Shop0Menu = $Shop0Menu
 
+var display_name : String = "THE SMITH"
+
 func _ready() -> void:
 	shop.exit.connect(func() -> void:
 		shop.close()
 		dialogue.show()
-		await dialogue.speak("THE SMITH", ["DON'T GET YOURSELF KILLED, NOW.","A FINE CHOICE FOR A FINE HUNTER!"][randi_range(0, 1)])
+		await dialogue.speak(display_name, ["DON'T GET YOURSELF KILLED, NOW.","A FINE CHOICE FOR A FINE HUNTER!"][randi_range(0, 1)])
 		close())
 
 func init() -> void:
-	if await dialogue.speak("THE SMITH", "HAIL, %s. WHAT WILL IT BE TODAY?" % SaveData.hunter_name, ["SHOP", "EXIT"]) == 0:
+	if SaveData.townsfolk & 4:
+		display_name = "GAVEIRAH, THE SMITH"
+	if await dialogue.speak(display_name, "HAIL, %s. WHAT WILL IT BE TODAY?" % SaveData.hunter_name, ["SHOP", "EXIT"]) == 0:
 		dialogue.hide()
 		shop.open()
 		return
